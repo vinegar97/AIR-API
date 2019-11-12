ccfed9adf2b5d8c518dc1af2e0a6ba267d4c9fcf
CarJem Generations <Cwallacecs@Gmail.com>
2019-11-12 12:58:47 -0500

    Inital Commit

---
+++ AIR-API/VersionMetadata.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace AIR_API
+{
+    public class VersionMetadata
+    {
+        public string FilePath = "";
+        private dynamic RawJSON;
+
+        public string VersionString;
+        public Version Version;
+        public string Game = "";
+        public string Author = "";
+        public VersionMetadata(FileInfo config)
+        {
+            FilePath = config.FullName;
+            string data = File.ReadAllText(FilePath);
+            RawJSON = Newtonsoft.Json.JsonConvert.DeserializeObject(data);
+            VersionString = RawJSON.Version;
+            Version = new Version(VersionString);
+            Game = RawJSON.Game;
+            Author = RawJSON.Author;
+        }
+    }
+}
